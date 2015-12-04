561408d148de4326079c0ac52c7cb47d5dab71ca
alvarpao <paola.alvarez@hp.com>
2015-12-04 12:28:46 -0800

    - Made Review class implement Parcelable

--- app/src/main/java/com/example/alvarpao/popularmovies/Review.java
+++ app/src/main/java/com/example/alvarpao/popularmovies/Review.java
@@ -1,9 +1,12 @@
 package com.example.alvarpao.popularmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by alvarpao on 11/18/2015.
  */
-public class Review {
+public class Review implements Parcelable{
 
     private String author;
     private String content;
@@ -13,6 +16,34 @@ public class Review {
         this.author = author;
         this.content = content;
     }
+
+    public Review(Parcel in){
+        author = in.readString();
+        content = in.readString();
+    }
+
+    public void writeToParcel(Parcel parcel, int content){
+        parcel.writeString(author);
+        parcel.writeString(this.content);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Parcelable.Creator<Review> CREATOR = new Parcelable.Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel parcel) {
+            return new Review(parcel);
+        }
+
+        @Override
+        public Review[] newArray(int i) {
+            return new Review[i];
+        }
+
+    };
 
     public String getAuthor(){ return author; }
     public String getContent(){ return content; }
